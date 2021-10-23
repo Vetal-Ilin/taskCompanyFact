c758a1efa861a5a9a1365252365b17e69edc2c3b
Vetal-Ilin <60708420+Vetal-Ilin@users.noreply.github.com>
2021-10-23 18:30:09 +0500

    Добавил fetch запрос при
    При рендере страницы добавил fetch запрос, который получает json с данными.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.scss';
 
 
 export default function App() {
+
+    const [arrayRequest, setArrayRequest] = useState([])
+
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/WilliamRu/TestAPI/master/db.json')
+         .then(res => res.json())
+         .then(res => setArrayRequest(res))
+         .catch(err => console.log(err))
+    }, [])
+
+    console.log(arrayRequest)
 
     return (
         <div className='app'>
