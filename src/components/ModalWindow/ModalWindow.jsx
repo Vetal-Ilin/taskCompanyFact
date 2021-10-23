1d1621f212349ed3aafa63bbed0d4f58b51dbb8c
Vetal-Ilin <60708420+Vetal-Ilin@users.noreply.github.com>
2021-10-24 01:11:05 +0500

    Сверстал компонент ModalWindow

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import './ModalWindow.scss';
+import iconError from '@images/iconError.svg';
 
 export default function ModalWindow(props) {
     return (
-        <div className={'modal-window ' + props.className}>
-            
-        </div>
+        <article className={'modal-window ' + props.className}>
+            <div className='modal-window__wrapper'>
+                <div className='modal-window__wrapper__card'>
+                    <div className='modal-window__wrapper__card__header'>
+                        <img src={iconError} alt='Ошибка' />
+                        <h1>Возникла ошибка</h1> 
+                    </div>
+                    <div className='modal-window__wrapper__card__message'>
+                        <p>Не удалось получить данные с сервера</p>
+                    </div>
+                    <div className='modal-window__wrapper__card__footer'>
+                        <button><p>ОК</p></button>
+                    </div>
+                </div>
+            </div>
+        </article>
     )
 }
