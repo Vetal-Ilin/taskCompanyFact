37ae62c1471de146c95b73c63252a139c2863d34
Vetal-Ilin <60708420+Vetal-Ilin@users.noreply.github.com>
2021-10-23 19:43:50 +0500

    Создал компонент ModalWindow
    Создал функциСоздал функциональный компонент, подключил к нему файл стилей ModalWindow.scss.

---
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ModalWindow.scss';
+
+export default function ModalWindow(props) {
+    return (
+        <div className={'modal-window ' + props.className}>
+            
+        </div>
+    )
+}
