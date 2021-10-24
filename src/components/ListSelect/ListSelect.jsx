4546f9e20082f7febadd608ff4db59e5209fbc3d
Vetal-Ilin <60708420+Vetal-Ilin@users.noreply.github.com>
2021-10-24 19:48:59 +0500

    Внес изменения в компонент ListSelect
    Добавил props addSelectedPropertyState и прокинул его дальше в Select.

--- src/components/ListSelect/ListSelect.jsx
+++ src/components/ListSelect/ListSelect.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { nanoid } from 'nanoid';
 import Select from '@components/Select/Select.jsx';
 
-export default function ListSelect({className, arrayDataSelect = []}) {
+export default function ListSelect({className, arrayDataSelect = [], addSelectedPropertyState}) {
 
     return (
         <div className={'list-select ' + className}>
             <h2>Выберите значения</h2>
             <div className='list-select__wrapper'>
-                {arrayDataSelect.map((item) => <Select options={item} key={nanoid()} />)}
+                {arrayDataSelect.map((item) => <Select options={item} key={nanoid()} addSelectedPropertyState={addSelectedPropertyState} />)}
             </div>
         </div>
     )
