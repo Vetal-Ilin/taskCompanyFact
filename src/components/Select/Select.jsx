8f019e27c224d068cada916940eb49885365b88b
Vetal-Ilin <60708420+Vetal-Ilin@users.noreply.github.com>
2021-10-24 19:48:27 +0500

    Внес изменения в компонент Select. 1) Добавил props addSelectedPropertyState 2) Добавил функцию clickOptions которая является колбеком на событие onChange 3) Добавил дефолтный option.

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { nanoid } from 'nanoid';
 
-export default function Select({name, options = []}) {
+export default function Select({name, options = [], addSelectedPropertyState}) {
+
+    const clickOptions = (event) => {
+        addSelectedPropertyState(event.target.value)
+    }
+
     return (
-        <select name={name} className='select'>
+        <select name={name} className='select' onChange={clickOptions} defaultValue={'DEFAULT'}>
+            <option value='DEFAULT' hidden>{typeof options[0]}</option>
             {options.map((item) => typeof item !== 'object' ? <option key={nanoid()}>{String(item)}</option> : <option key={nanoid()}>{JSON.stringify(item)}</option>)}
         </select>
     )
