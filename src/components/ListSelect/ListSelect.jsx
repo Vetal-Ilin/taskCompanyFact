f475b9d5bffbaeb0a00311edd346df4cc1e7cd90
Vetal-Ilin <60708420+Vetal-Ilin@users.noreply.github.com>
2021-10-24 18:02:09 +0500

    Сделал изменения в компоненте ListSelect. Прописал props, а так же добавил перебор массива и рендеринг на страницу Select исходя из принятого массива props.

--- src/components/ListSelect/ListSelect.jsx
+++ src/components/ListSelect/ListSelect.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { nanoid } from 'nanoid';
+import Select from '@components/Select/Select.jsx';
 
-export default function ListSelect({className = '', arrayDataSelect = []}) {
-    console.log(arrayDataSelect)
+export default function ListSelect({className, arrayDataSelect = []}) {
+
     return (
         <div className={'list-select ' + className}>
-            
+            {arrayDataSelect.map((item) => <Select options={item} key={nanoid()} />)}
         </div>
     )
 }
