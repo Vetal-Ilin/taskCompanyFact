23e3d976b5407fee1ebc21e928bf1e36d68baea9
Vetal-Ilin <60708420+Vetal-Ilin@users.noreply.github.com>
2021-10-24 13:50:20 +0500

    Добавил функцию для сглаживания массива

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,25 +5,35 @@ import ModalWindow from '@components/ModalWindow/ModalWindow.jsx';
 export default function App() {
 
     const [arrayRequest, setArrayRequest] = useState([]);
-    const [showModalWindowError, setShowModalWindowError] = useState(false)
+    const [showModalWindowError, setShowModalWindowError] = useState(false);
+    let smoothedАrray = [];
+
+    const customFlatArrMethod = (arr) => {
+        for (let item of arr) {
+           if(Array.isArray(item)) {
+               customFlatArrMethod(item)
+           } else {
+                smoothedАrray.push(item)
+           }
+        }
+    }
 
     async function loadJson(url) {
         let response = await fetch(url);
-
         if (response.status === 200) {
             let json = await response.json();
             let resultingArray = [];
             for (let item in json) {
                 resultingArray.push(json[item])
             } 
-            setArrayRequest(resultingArray);
+            setArrayRequest(resultingArray); //
+            customFlatArrMethod(resultingArray);
             return
         }
-
         throw new Error(response.status);
     }
 
-    let closeModalWindowError = () => {
+    const closeModalWindowError = () => {
         setShowModalWindowError(false)
     }
 
