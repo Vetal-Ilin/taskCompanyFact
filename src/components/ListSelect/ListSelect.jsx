5c36f9e446cb4f08cb32acb04aa13cefcd41fa9e
Vetal-Ilin <60708420+Vetal-Ilin@users.noreply.github.com>
2021-10-24 16:11:08 +0500

    Добавил props className в компонент ListSelect.

--- src/components/ListSelect/ListSelect.jsx
+++ src/components/ListSelect/ListSelect.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function ListSelect() {
+export default function ListSelect({className = ''}) {
     return (
-        <div className='list-select'>
+        <div className={'list-select ' + className}>
             
         </div>
     )
