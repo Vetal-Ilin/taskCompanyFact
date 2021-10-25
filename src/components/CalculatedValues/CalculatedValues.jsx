7ecac48b2f2c67d766a527ddcba0dbc00c304da7
Vetal-Ilin <60708420+Vetal-Ilin@users.noreply.github.com>
2021-10-25 23:30:09 +0500

    Внес изменения в компонент.

--- src/components/CalculatedValues/CalculatedValues.jsx
+++ src/components/CalculatedValues/CalculatedValues.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function CalculatedValues({className, selectedMultipliedPropertiesNumbers, title}) {
+export default function CalculatedValues({className, calculatedValue, title}) {
     return (
         <div className={'calculated-values ' + className}>
             <p>{title}:</p>
-            {selectedMultipliedPropertiesNumbers ? <p>{selectedMultipliedPropertiesNumbers}</p> : <p>Данные не выбраны</p>}
+            {calculatedValue && calculatedValue != '' ? <p>{calculatedValue}</p> : <p>Данные не выбраны</p>}
         </div>
     )
 }
