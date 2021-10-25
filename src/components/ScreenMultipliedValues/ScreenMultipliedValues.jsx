22d21a794ac9fca3acd853980d392a0ae872891f
Vetal-Ilin <60708420+Vetal-Ilin@users.noreply.github.com>
2021-10-25 20:34:10 +0500

    Создал компонент ScreenMultipliedValues, и подключил его стили.

---
+++ src/components/ScreenMultipliedValues/ScreenMultipliedValues.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ScreenMultipliedValues() {
+    return (
+        <div className='screen-multiplied-values'>
+            
+        </div>
+    )
+}
