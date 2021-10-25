b1fff1d519a0a4a6474f5e377b22b7fa9006ae9f
Vetal-Ilin <60708420+Vetal-Ilin@users.noreply.github.com>
2021-10-25 22:03:17 +0500

    Принял props в компоненте ScreenMultipliedValues

--- src/components/ScreenMultipliedValues/ScreenMultipliedValues.jsx
+++ src/components/ScreenMultipliedValues/ScreenMultipliedValues.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function ScreenMultipliedValues() {
+export default function ScreenMultipliedValues({selectedMultipliedPropertiesNumbers}) {
     return (
         <div className='screen-multiplied-values'>
-            
+            <p>{selectedMultipliedPropertiesNumbers}</p>
         </div>
     )
 }
