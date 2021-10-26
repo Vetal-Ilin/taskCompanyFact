9cad9ad76673084005521dd65d964a04a873cbf9
Vetal-Ilin <60708420+Vetal-Ilin@users.noreply.github.com>
2021-10-27 00:27:16 +0500

    src/components/ListSelect/ListSelect.jsx

--- src/components/ListSelect/ListSelect.jsx
+++ src/components/ListSelect/ListSelect.jsx
@@ -3,7 +3,6 @@ import { nanoid } from 'nanoid';
 import Select from '@components/Select/Select.jsx';
 
 export default function ListSelect({className, arrayDataSelect = [], addSelectedPropertyState}) {
-
     return (
         <div className={'list-select ' + className}>
             <h2>Выберите значения</h2>
