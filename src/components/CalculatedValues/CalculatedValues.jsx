ac46aa7a52e79dc16d826ef8d0fe5a43d1043565
Vetal-Ilin <60708420+Vetal-Ilin@users.noreply.github.com>
2021-11-06 21:22:59 +0500

    внес изменения в компонент CalculatedValues

--- src/components/CalculatedValues/CalculatedValues.jsx
+++ src/components/CalculatedValues/CalculatedValues.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function CalculatedValues({className, calculatedValue, title}) {
     return (
-        <div className={'calculated-values ' + className}>
+        <article className={'calculated-values ' + className}>
             <p>{title}:</p>
             {calculatedValue && calculatedValue != '' ? <p>{calculatedValue}</p> : <p>Данные не выбраны</p>}
-        </div>
+        </article>
     )
 }
