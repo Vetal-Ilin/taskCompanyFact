257861b2c79856785f6e0d29dd1558be4a5c03dd
Vetal-Ilin <60708420+Vetal-Ilin@users.noreply.github.com>
2021-11-06 15:29:12 +0500

    внес изменения в компонент ModalWindow

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -2,22 +2,26 @@ import React from 'react';
 import iconError from '@images/iconError.svg';
 
 export default function ModalWindow(props) {
-    return (
-        <article className={'modal-window ' + props.className}>
-            <div className='modal-window__wrapper'>
-                <div className='modal-window__wrapper__card'>
-                    <div className='modal-window__wrapper__card__header'>
-                        <img src={iconError} alt='Ошибка' />
-                        <h1>Возникла ошибка</h1> 
-                    </div>
-                    <div className='modal-window__wrapper__card__message'>
-                        <p>Не удалось получить данные с сервера</p>
-                    </div>
-                    <div className='modal-window__wrapper__card__footer'>
-                        <button onClick={props.closeModalWindowError}><p>ОК</p></button>
+    if(props.showModalWindowError) {
+        return (
+            <article className={'modal-window ' + props.className}>
+                <div className='modal-window__wrapper'>
+                    <div className='modal-window__wrapper__card'>
+                        <div className='modal-window__wrapper__card__header'>
+                            <img src={iconError} alt='Ошибка' />
+                            <h1>Возникла ошибка</h1> 
+                        </div>
+                        <div className='modal-window__wrapper__card__message'>
+                            <p>Не удалось получить данные с сервера</p>
+                        </div>
+                        <div className='modal-window__wrapper__card__footer'>
+                            <button onClick={props.closeModalWindowError} id='modal-window-button-OK-id'><p>ОК</p></button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </article>
-    )
+            </article>
+        )
+    } else {
+       return null
+    }
 }
